21b2c28a9cfbd114a7f1b0633b45b8a4cc893bda
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-03-03 14:54:22 +0200

    add form

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './form.css';
 
- const Form= () => {
-   return null;
+const Form = () => {
+  
+  const onChangeHandler = e => {
+    console.log(e)
+  }
+
+   return (
+     <form>
+       <h1>My first form in react</h1>
+       <label htmlFor='firstName'>First name</label>
+       <input id='firstName'/>
+     </form>
+   );
 }
 
 export default Form;
